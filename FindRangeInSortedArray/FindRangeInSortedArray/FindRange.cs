8965f7afdc0e7835b4615a1bd0b35f986808c7fa
Surajit Ghosh <Surajit.1986@gmail.com>
2023-09-11 01:13:30 +0530

    find range of a target in a sorted array

---
+++ FindRangeInSortedArray/FindRangeInSortedArray/FindRange.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindRangeInSortedArray
+{
+    internal class FindRange
+    {
+        public int[] FindRangeFunc(int[] arr, int target)
+        {
+            int firstInd;
+            int lastInd;
+
+            if(arr.Length > 0)
+            {
+                firstInd = FindFirstIndex(arr, target);
+                lastInd = FindLastIndex(arr,target);
+
+                return new int[] {firstInd, lastInd };
+            }
+
+
+            return new int[] { - 1, -1};
+        }
+
+        private int FindFirstIndex(int[] arr, int target)
+        {
+            int left = 0;
+            int right = arr.Length - 1;
+            int mid = 0;
+
+            while(left<=right)
+            {
+                mid = (left+right)/2;
+
+                if (arr[mid] == target)
+                {
+                    if((mid ==0) || (arr[mid-1] != target))
+                    {
+                        return mid;
+                    }
+                    right = mid - 1;
+                }
+                else if (arr[mid] > target)
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+
+            }
+
+            return -1;
+        }
+
+        private int FindLastIndex(int[] arr, int target)
+        {
+            int left = 0;
+            int right = arr.Length - 1;
+            int mid = 0;
+
+            while (left <= right)
+            {
+                mid = (left + right) / 2;
+
+                if (arr[mid] == target)
+                {
+                    if ((mid == (arr.Length-1)) || (arr[mid + 1] != target))
+                    {
+                        return mid;
+                    }
+                    left = mid + 1;
+                }
+                else if (arr[mid] > target)
+                {
+                    right = mid -1;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ FindRangeInSortedArray/FindRangeInSortedArray/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FindRangeInSortedArray;
+
+Console.WriteLine("Hello, World!");
+
+int[] arr = new int[] { 10, 11, 11, 11, 14, 15 };
+int target = 11;
+FindRange obj = new FindRange();
+
+int[] result = obj.FindRangeFunc(arr, target);
+Console.Write($"Start Index: {result[0]}, Last Index:{result[1]}");
+Console.ReadKey();
